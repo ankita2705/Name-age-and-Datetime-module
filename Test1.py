ee6bad2b23438addf9adfb2c7184c247d262bd79
ankita2705 <ankitathomas2705@gmail.com>
2017-11-14 22:07:27 -0500

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+now=datetime.now()
+name=input("Enter Name: ")
+age=int(input("Enter the age: "))
+
+no_of_years = 100-age
+
+year=now.year
+
+print("After "+str(no_of_years)+ " your age will be 100 in "+str(year+no_of_years))
+
+num=int(input("Enter a number: "))
+for i in range(num):
+    print("After "+str(no_of_years)+ " your age will be 100 \n")
+    
+
+
+
+
+
